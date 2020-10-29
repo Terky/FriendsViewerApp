97ecfb60c8b65c0c1baed0a0962660cccd1b24cc
Terky <artemab@outlook.com>
2020-10-29 19:24:16 +0300

    Initial Commit

---
+++ Friends Viewer AppTests/Friends_Viewer_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Friends_Viewer_AppTests.swift
+//  Friends Viewer AppTests
+//
+//  Created by Artem Burmistrov on 10/29/20.
+//
+
+import XCTest
+@testable import Friends_Viewer_App
+
+class Friends_Viewer_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
